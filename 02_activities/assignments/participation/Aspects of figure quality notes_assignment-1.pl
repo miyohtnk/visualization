0145d9f1868150cbcd4d1e3c63219fff1d7e99ec
miyohtnk <miyohtnk@gmail.com>
2025-04-22 20:31:17 -0400

    Added jupyter file and text notes

---
+++ 02_activities/assignments/participation/Aspects of figure quality notes_assignment-1.pl
@@ -0,0 +1,19 @@
+Aspects of my figure that match qualities of data visualizations
+
+
+Aesthetic: The plot is pleasing to look at, 
+with colors that make sense, and it is not too dense with information or
+jargon
+
+
+Substantive:The data was retrieved from CRA, so this is the accurate
+historical data. A legend with this information is supplied in the figure.
+
+
+Perceptual: It is easy to see that in the summer months the temperatures
+have been higher than in the winter months, and we also see the spread 
+of the temperatures, showing how it varies from year to year.
+Unfortunately we cannot tell if later years correspond to generally higher
+temperatures.
+
+
